3d7ca73448c9d154b3baab2387922a69730be06c
sebsu <sebsu243@gmail.com>
2016-12-03 14:51:16 +0100

    Completed part 2 of day 3

--- AdventOfCode2016/Day3/d3.1_squares.cpp
+++ AdventOfCode2016/Day3/d3.1_squares.cpp
@@ -7,34 +7,29 @@
 using namespace std;
 
 int main(){
-    string instrLine, instr;
-    signed long int numTri = 0;
+    string instrLine;
+    unsigned long int numTri = 0;
     // Get the entire line
     getline(cin, instrLine);
     while (!instrLine.empty()){
         int side1 = 0;
         int side2 = 0;
         int side3 = 0;
-        //instr = instrLine;
-        //while (!instr.empty()){
+
         instrLine.erase(0, instrLine.find_first_of("0123456789"));
-        
         int numLen = instrLine.find_first_of(' ');
         string side1Str = instrLine.substr(0,numLen);
+        
         instrLine.erase(0, instrLine.find_first_of("0123456789", numLen));
-        //instrLine.erase(0, min(numLen+1,(int)instrLine.length()));
         numLen = instrLine.find_first_of(' ');
         string side2Str = instrLine.substr(0,numLen);
+        
         instrLine.erase(0, instrLine.find_first_of("0123456789", numLen));
-        //instrLine.erase(0, min(numLen+1,(int)instrLine.length()));
-        //numLen = instrLine.find_first_of(' ');
-        //string side3Str = instrLine.substr(0,numLen);
         string side3Str = instrLine;
-        //instrLine.erase(0, min(numLen+1,(int)instrLine.length()));
-        //}
-        side1 = std::stoi(side1Str,nullptr);
-        side2 = std::stoi(side2Str,nullptr);
-        side3 = std::stoi(side3Str,nullptr);
+
+        side1 = stoi(side1Str,nullptr);
+        side2 = stoi(side2Str,nullptr);
+        side3 = stoi(side3Str,nullptr);
 
         if (side1+side2 > side3 && side1+side3 > side2 && side2+side3 > side1){
             ++numTri;
--- AdventOfCode2016/Day3/d3.2_vert.cpp
+++ AdventOfCode2016/Day3/d3.2_vert.cpp
@@ -7,16 +7,16 @@
 using namespace std;
 
 int main(){
-    string instrLine, instr;
-    signed long int numTri = 0;
+    string instrLine;
+    unsigned long int numTri = 0;
     int side = 0;
-    int tri1[] = {};
-    int tri2[] = {};
-    int tri3[] = {};
+    int tri1[3] = {};
+    int tri2[3] = {};
+    int tri3[3] = {};
     // Get the entire line
     getline(cin, instrLine);
     while (!instrLine.empty()){
-
+        // Extract the numbers and store them as strings.
         instrLine.erase(0, instrLine.find_first_of("0123456789"));
         int numLen = instrLine.find_first_of(' ');
         string side1Str = instrLine.substr(0,numLen);
@@ -28,24 +28,28 @@ int main(){
         instrLine.erase(0, instrLine.find_first_of("0123456789", numLen));
         string side3Str = instrLine;
 
-        tri1[side] = std::stoi(side1Str,nullptr);
-        tri2[side] = std::stoi(side2Str,nullptr);
-        tri3[side] = std::stoi(side3Str,nullptr);
+        // Convert the strings to ints.
+        tri1[side] = stoi(side1Str, nullptr);
+        tri2[side] = stoi(side2Str, nullptr);
+        tri3[side] = stoi(side3Str, nullptr);
 
-        if (side == 2){
+        if (side == 2){ // Once all three sides has been read, check if valid.
             if (tri1[0]+tri1[1] > tri1[2] &&
                 tri1[0]+tri1[2] > tri1[1] &&
                 tri1[1]+tri1[2] > tri1[0]){
+                //                cout << tri1[0] << " " << tri1[1] << " " << tri1[2] << endl;
                 ++numTri;
             }
             if (tri2[0]+tri2[1] > tri2[2] &&
                 tri2[0]+tri2[2] > tri2[1] &&
                 tri2[1]+tri2[2] > tri2[0]){
+                //cout << tri2[0] << " " << tri2[1] << " " << tri2[2] << endl;
                 ++numTri;
             }
             if (tri3[0]+tri3[1] > tri3[2] &&
                 tri3[0]+tri3[2] > tri3[1] &&
                 tri3[1]+tri3[2] > tri3[0]){
+                //cout << tri3[0] << " " << tri3[1] << " " << tri3[2] << endl;
                 ++numTri;
             }
         }
