97d33d7abfe3b55afc6345b25be352f417e34058
sebsu <sebsu243@gmail.com>
2018-12-03 23:46:28 +0100

    Day3, part 2 done

--- AdventOfCode2018/Day3/d3_2_unique.rs
+++ AdventOfCode2018/Day3/d3_2_unique.rs
@@ -7,20 +7,28 @@ fn main() {
     let mut file = File::open("input.txt").expect("File not found!");
     file.read_to_string(&mut input)
         .expect("Failed to read file.");
-    let mut fabric: HashMap<String, u32> = HashMap::new();
+    let mut fabric: HashMap<String, Vec<u32>> = HashMap::new();
+    let mut unique: HashMap<u32, u32> = HashMap::new();
 
     for line in input.lines() {
         let (mut coord, mut size) = line.split_at(line.find(":")
                                                   .expect("Can't find ':'"));
-        let (_, coord) = coord.split_at(coord.find("@ ")
-                                        .expect("Can't find '@ '") + 2);
-        size = size.trim_start_matches(": ");
-
+        let (mut id_str, coord) = coord.split_at(coord.find("@ ")
+                                                 .expect("Can't find '@ '") + 2);
+        
+        // Getting the claim id and enters it into a hashmap for later
+        id_str = id_str.trim().trim_matches('#').trim_matches('@').trim();
+        let id = id_str.parse::<u32>().unwrap();
+        unique.insert(id, 1);
+        
+        // Getting the starting coordinates as ints
         let (sx_str, mut sy_str) = coord.split_at(coord.find(",")
                                                   .expect("Can't find ','"));
         let mut sx = sx_str.parse::<u32>().unwrap();
         let mut sy = sy_str.trim_start_matches(",").parse::<u32>().unwrap();
-
+        
+        // Getting the patch size as ints
+        size = size.trim_start_matches(": ");
         let (nx_str, mut ny_str) = size.split_at(size.find("x")
                                                  .expect("Can't find 'x'"));
         let mut nx = nx_str.parse::<u32>().unwrap();
@@ -32,23 +40,25 @@ fn main() {
             x_str.push(',');
             for j in 0..ny {
                 let y = sy + j;
+                // Saving the coord as a string for unique keys in the hashmap
                 let mut coord_str = x_str.to_owned();
                 coord_str.push_str(&y.to_string());
-                let mut n = match fabric.get(&coord_str) {
-                    Some(val) => *val,
-                    None => 0,
-                };
-                fabric.insert(coord_str, n + 1);
+                // Enter the id into a vec of claims that has claimed that spot
+                fabric.entry(coord_str).or_insert_with(Vec::new).push(id);
             }
         }
     }
 
-    let mut overlap = 0;
-    for key in fabric.keys() {
-        let count = fabric.get(key).expect("No such key");
-        if *count > 1 {
-            overlap = overlap + 1;
+    for coord in fabric.keys() {
+        let visited = fabric.get(coord).expect("No such key");
+        if visited.len() > 1 { // If more than one has claimed this spot
+            for id in visited { // none of the claims are valid.
+                unique.remove(id);
+            }
         }
     }
-    println!("Overlapping {}", overlap);
+
+    for id in unique.keys() { // Should be only one, but why not print all
+        println!("Claim with no overlap: {}", id);
+    }
 }
