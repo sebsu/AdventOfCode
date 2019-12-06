282a26eef7b2008c55a2e11f82945e9e69684a9d
sebsu <sebsu@users.noreply.github.com>
2019-12-06 23:57:48 +0100

    2019, day 6, C, complete

--- AdventOfCode2019/Day06/d6_2_santa.c
+++ AdventOfCode2019/Day06/d6_2_santa.c
@@ -1,30 +1,65 @@
 #include <assert.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_HASH 46656 /* 36^3 */
 #define SIZE 2048
-static int orbits[1000000][SIZE];
+#define MAX_DEPTH 262144
+static int orbits[MAX_HASH][SIZE];
+static int path_you[MAX_DEPTH];
+static int path_san[MAX_DEPTH];
 
 int get_hash(char *str, int pos) {
   int out = 0;
+  int power = 1;
+  int base = 36;
   for (int i = pos; i < pos + 3; ++i) {
-    out = (out * 100) + str[i];
+      char c = (str[i]>'9') ? (str[i]-'A'+10) : (str[i]-'0');
+      out += c * power;
+      power = power * base;
   }
   return out;
 }
 
-int *find(int hash, int goal, int *path, int depth) {
-  if (hash == goal) {
-    return path;
-  }
-  path[depth] = hash;
-  int n_sat = orbits[hash][0];
-  for (int i = 1; i < n_sat + 1; ++i) {
-    return find(orbits[hash][i], goal, path, depth + 1);
-  }
-  return path; /*Shouldn't reach this */
+int HASH_YOU;
+int HASH_SAN;
+int HASH_COM;
+
+void set() {
+    HASH_YOU = get_hash("YOU", 0);
+    HASH_SAN = get_hash("SAN", 0);
+    HASH_COM = get_hash("COM", 0);
+}
+
+int find(int goal) {
+    int pos = 0;
+    int sat = goal;
+    while (sat != HASH_COM) {
+        bool found = false;
+        for (int i=0; i<MAX_HASH; ++i) {
+            int n_sat = orbits[i][0];
+            for (int j=1; j<n_sat+1; ++j) {
+                if (orbits[i][j] == sat) {
+                    if (goal == HASH_YOU) {
+                        path_you[pos++] = sat;
+                    } else if (goal == HASH_SAN) {
+                        path_san[pos++] = sat;
+                    }
+                    found = true;
+                    sat = i;
+                    break;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+    }
+    
+    return pos;
 }
 
 int main(int argc, char **argv) {
@@ -34,7 +69,7 @@ int main(int argc, char **argv) {
   char str[SIZE];
   f = fopen(argv[1], "r");
 
-  for (int i = 0; i < 1000000; ++i) {
+  for (int i = 0; i < MAX_HASH; ++i) {
     orbits[i][0] = 0;
   }
 
@@ -44,14 +79,31 @@ int main(int argc, char **argv) {
     int n_sat = orbits[steady][0] + 1;
     orbits[steady][n_sat] = satellite;
     orbits[steady][0] = n_sat;
+    /* printf("%i)%i, %s", steady, satellite,  str); */
   }
 
-  static int path[100];
-  int *a = find(get_hash("COM", 0), get_hash("YOU", 0), path, 0);
-  int *b = find(get_hash("COM", 0), get_hash("SAN", 0), path, 0);
+  set();
+  int depth_you = find(HASH_YOU);
+  int depth_san = find(HASH_SAN);
+
+  /* for (int i = 0; i<depth_you; ++i){ */
+  /*     printf("%i, ", path_you[i]); */
+  /* } */
+  /* printf("\n"); */
+  /* for (int i = 0; i<depth_san; ++i){ */
+  /*     printf("%i, ", path_san[i]); */
+  /* } */
+
+  int i=depth_you;
+  int j=depth_san;
+  while (path_you[i]==path_san[j] && i>=0 && j>=0){
+      --i;
+      --j;
+  }
 
   fclose(f);
-  printf("Result: %i\n", 0);
+  printf("Result: %i\n", i+j);
+  /* printf("Result: %i\n", get_hash("ZZZ", 0)); */
 
   return 0;
 }
